095b2cd161859bb4e0337c9be3f66492921fbb39
Will Gundy <willgunderson503@gmail.com>
2022-03-01 11:23:52 -0800

    initial commit of about me page

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // import functions
 
 // grab DOM elements
-
+const showButton = document.getElementById('favorite-animal-btn');
+const animal = document.getElementById('animalDiv');
 // set event listeners 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+showButton.addEventListener('click',
+    () => {
+        animal.classList.remove('hidden');
+    });
+
