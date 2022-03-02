bd99770313d0b117080cb249ee905fd3ef57c8ba
Will Gundy <willgunderson503@gmail.com>
2022-03-01 16:28:31 -0800

    Add in ordered list and clean up Two Truths Div

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 const animalButton = document.getElementById('favorite-animal-btn');
 const animal = document.getElementById('animalDiv');
 const lieButton = document.getElementById('the-lie-btn');
-const theLie = document.getElementById('lieText');
 // set event listeners 
     // get user input
     // use user input to update state 
@@ -16,15 +15,11 @@ animalButton.addEventListener('click',
 
 lieButton.addEventListener('click',
     () => {
-        theLie.classList.toggle('hidden');
         const lie = document.getElementById('lie');
-        const truth1 = document.getElementById('truth1');
-        const truth2 = document.getElementById('truth2');
         lie.classList.toggle('lie-text');
-        console.log(truth1);
-        console.log(truth2);
-        truth1.classList.toggle('truth-text');
-        truth2.classList.toggle('truth-text');
+        const truth = document.querySelectorAll('.truth');
+        truth[0].classList.toggle('truth-text');
+        truth[1].classList.toggle('truth-text');
         if (lieButton.innerHTML === 'Show Me The Lie...') {
             lieButton.innerHTML = 'Hide the Lie!';
         } else {
